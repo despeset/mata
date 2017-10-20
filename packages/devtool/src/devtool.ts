f7873040964c52d21a529b04227b4ad4eb3d72ad
Daniel Espeset <daniel@danielespeset.com>
2017-10-20 06:35:04 -0400

    Welp more sketches

--- packages/devtool/src/devtool.ts
+++ packages/devtool/src/devtool.ts
@@ -9,20 +9,26 @@ function style(target: HTMLElement, styles: { [prop: string]: number | string })
 	Object.keys(styles).forEach(prop => target.style[<any>prop] = <any>styles[prop]);
 }
 
+export type Inner<T> = T
+
 export class Panel {
 	frame: HTMLIFrameElement;
-	window: Window;
-	container: HTMLDivElement;
-	toolbar: HTMLDivElement;
-	body: HTMLBodyElement;
+	mask: HTMLDivElement;
+	resizer: HTMLDivElement;
+	onResize: (() => void) | null;
+	innerWindow: Inner<Window>;
+	container: Inner<HTMLDivElement>;
+	toolbar: Inner<HTMLDivElement>;
+	body: Inner<HTMLBodyElement>;
 	x: number;
 	y: number;
-	lastClientX: number | null;
-	lastClientY: number | null;
+	prevTarget: { x: number, y: number } | null;
 
 	static styles = {
 		body: {
-			margin: 0
+			margin: 0,
+			width: '100%',
+			height: '100%',
 		},
 		toolbar: {
 			width: '100em',
@@ -33,73 +39,131 @@ export class Panel {
 			border: '1px solid pink',
 			position: 'absolute',
 			boxShadow: '1px 1px 2px pink',			
+		},
+		mask: {
+			zIndex: 99,
+			position: 'absolute',
+		},
+		resizer: {
+			height: '20px',
+			width: '20px',
+			position: 'absolute',
+			right: 0,
+			bottom: 0,
+			background: 'blue',
 		}
 	}
 
 	constructor(parent: HTMLElement, size: Size) {
 		this.frame = document.createElement('iframe');
+		this.mask = document.createElement('div');
 		parent.appendChild(this.frame);
-		this.window = this.frame.contentWindow;
-		this.container = this.window.document.createElement('div');
-		this.toolbar = this.renderToolbar();
-		this.body = <HTMLBodyElement>this.window.document.body;
-		this.body.appendChild(this.toolbar);
+		parent.appendChild(this.mask);
+		this.innerWindow = this.frame.contentWindow;
+		this.container = this.innerWindow.document.createElement('div');
+		this.body = <Inner<HTMLBodyElement>>this.innerWindow.document.body;
+		this.body.appendChild(this.renderToolbar(this.innerWindow));
 		this.body.appendChild(this.container);
+
+		this.toolbar = this.renderToolbar(window);
+		style(this.toolbar, {
+			opacity: '0'
+		});
+		this.mask.appendChild(this.toolbar);
+
+		this.resizer = window.document.createElement('div');
+		style(this.resizer, Panel.styles.resizer);
+		this.mask.appendChild(this.resizer);
+		
 		this.frame.width  = size.width + 'px';
 		this.frame.height = size.height + 'px';
+		this.mask.style.width = size.width + 'px';
+		this.mask.style.height = size.height + 'px';
+
+		style(window.document.body, Panel.styles.body);
 		style(this.body, Panel.styles.body);
 		style(this.frame, Panel.styles.frame);
+		style(this.mask, Panel.styles.mask);
+		
 		this.x = 0;
 		this.y = 0;
 
-		this.drag = this.drag.bind(this);
+		this.dragMove = this.dragMove.bind(this);
+		this.dragResize = this.dragResize.bind(this);
+		this.startDragging = this.startDragging.bind(this);
+		this.stopDragging = this.stopDragging.bind(this);
+		this.onResize = null;
 		
-		this.toolbar.addEventListener('mousedown', _ => {
-			console.log("DOWN");
-			style(this.frame, {
-				pointerEvents: 'none',
-				boxShadow: '3px 3px 10px pink',
-			});
-			// this.body.addEventListener('mousemove', this.drag);
-			setTimeout(() => {
-				window.document.addEventListener('mousemove', this.drag);
-			}, 0);
-		});
-		window.document.addEventListener('mouseup', _ => {
-			console.log("UP");
-			// this.body.removeEventListener('mousemove', this.drag);
-			style(this.frame, {
-				pointerEvents: 'auto',
-				boxShadow: '1px 1px 2px pink',				
-			});
-			this.lastClientX = null;
-			this.lastClientY = null;
-			window.document.removeEventListener('mousemove', this.drag);
-		});
+		this.toolbar.addEventListener('mousedown', this.startDragging(this.dragMove).bind(this));
+
+		this.resizer.addEventListener('mousedown', this.startDragging(this.dragResize).bind(this));
+
 	}
 
-	renderToolbar() {
-		const { container, window } = this;
-		const div = window.document.createElement('div');
-		container.appendChild(div);
+	renderToolbar(context: Window | Inner<Window>) {
+		const div = context.document.createElement('div');
 		style(div, Panel.styles.toolbar);
 		return div;
 	}
 
-	drag(e: MouseEvent) {
-		this.lastClientX = this.lastClientX || e.clientX;
-		this.lastClientY = this.lastClientY || e.clientY;
-		this.x += (e.clientX - this.lastClientX);
-		this.y += (e.clientY - this.lastClientY);
-		this.lastClientX = e.clientX;
-		this.lastClientY = e.clientY;
+	startDragging(dragHandler: (e: MouseEvent) => void):  () => void {
+		return () => {
+			style(this.frame, {
+				boxShadow: '0px 20px 50px lightpink',				
+				userSelect: 'none',
+			});
+			window.document.body.addEventListener('mouseup', this.stopDragging(dragHandler).bind(this));
+			window.document.body.addEventListener('mousemove', dragHandler);
+		}
+	}
+
+	stopDragging(dragHandler: (e: MouseEvent) => void): () => void {
+		const handler = () => {			
+			style(this.frame, {
+				boxShadow: '1px 1px 2px pink',			
+			});
+			this.prevTarget = null;		
+			window.document.body.removeEventListener('mouseup', handler);
+			window.document.body.removeEventListener('mousemove', dragHandler);
+		}
+		return handler;
+	}
+
+	dragResize(e: MouseEvent) {
+		const target = { x: e.clientX, y: e.clientY };
+		const width = (target.x - this.frame.offsetLeft) + 10 + 'px';
+		const height = (target.y - this.frame.offsetTop) + 10 + 'px';
+		this.frame.width = width;
+		this.frame.height = height;
+		style(this.mask, { width, height });
+		if (this.onResize) {
+			this.onResize();
+		}
+	}
+	
+	dragMove(e: MouseEvent) {
+		const target = { x: e.clientX, y: e.clientY };
+		this.prevTarget = this.prevTarget || target;
+		this.x += (target.x - this.prevTarget.x);
+		this.y += (target.y - this.prevTarget.y);
+		this.prevTarget = target;
 		this.x = this.x < 0 ? 0 : this.x;
 		this.y = this.y < 0 ? 0 : this.y;
-		console.log([this.x, this.y]);
-		style(this.frame, {
-			left: this.x + 'px',
-			top: this.y + 'px'
-		});
+		if (this.x < 10 || this.y < 10) {
+			setTimeout(() => {
+				this.stopDragging(this.dragMove);
+			})
+		}
+		// hmm: setTimeout(() => {
+			style(this.frame, {
+				left: this.x + 'px',
+				top: this.y + 'px',
+			});
+			style(this.mask, {
+				left: this.x + 'px',
+				top: this.y + 'px',
+			})	
+		// }, 0)
 	}
 	
 }
@@ -113,6 +177,7 @@ export class Inspector {
 	g: dagreD3.graphlib.Graph;
 	render: dagreD3.Render;
 	panel: Panel;
+	zoom: d3.behavior.Zoom<{}>;
 	styles: { [k:string]: { [k:string]: string }};
 
 	constructor(parent: HTMLElement, fsm: Mata.Automaton<any>, size: Size = { width: 200, height: 200 }) {
@@ -124,6 +189,7 @@ export class Inspector {
 		fsm.subscribe(this.update.bind(this));
 		this.$el = document.createElement('div');
 		this.panel.container.appendChild(this.$el);
+		this.panel.onResize = this.zoomToFit.bind(this);
 		const machine = fsm.schematic.rules;
 		const levelA = Object.keys(fsm.schematic.rules);
 		const g = this.g = new dagreD3.graphlib.Graph();
@@ -219,7 +285,7 @@ export class Inspector {
 			node.rx = node.ry = 5;
 			node.style = "fill: #C7FFEA";
 		});
-		var zoom = d3.behavior.zoom().on("zoom", function() {
+		var zoom = this.zoom = d3.behavior.zoom().on("zoom", function() {
 			svg.select("g")
 			.attr(
 				"transform", 
@@ -231,24 +297,31 @@ export class Inspector {
 
 		svg.call(render, g);
 		
+		this.zoomToFit();
+
+		// initStaticForceLayout(<d3.Selection<SVGSVGElement, SVGSVGElement, null, undefined>>svg, this.nodes, this.links, this.fsm);
+	}
+
+	zoomToFit() {
+		const { g, svg, zoom } = this;
+		svg.attr('width', this.panel.frame.width);
+		svg.attr('height', parseInt(this.panel.frame.height) - 20 + 'px');
 		// Center the dag
-		var zoomScale = 1;
+		let zoomScale = 1;
 		// Get Dagre Graph dimensions
-		var graphWidth = g.graph().width;
-		var graphHeight = g.graph().height + 0;
+		let graphWidth = g.graph().width;
+		let graphHeight = g.graph().height + 0;
 		// Get SVG dimensions
-		var width = parseInt(svg.style("width").replace(/px/, ""));
-		var height = parseInt(svg.style("height").replace(/px/, ""));
+		let width = parseInt(svg.style("width").replace(/px/, ""));
+		let height = parseInt(svg.style("height").replace(/px/, ""));
 
 		// Calculate applicable scale for zoom
 		zoomScale = Math.max( Math.min(width / graphWidth, height / graphHeight), 0.5);
 
-		var translate: [number, number] = [(width/2) - ((graphWidth*zoomScale)/2), 0];
+		let translate: [number, number] = [(width/2) - ((graphWidth*zoomScale)/2), 0];
 		zoom.translate(translate);
 		zoom.scale(zoomScale);
 		zoom.event(svg.select('g'));
-
-		// initStaticForceLayout(<d3.Selection<SVGSVGElement, SVGSVGElement, null, undefined>>svg, this.nodes, this.links, this.fsm);
 	}
 
 	private update(event: Mata.TransitionEvent<any>) {
